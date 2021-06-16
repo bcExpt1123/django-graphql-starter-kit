1f4e6dc5d96d25ace4ad76e6d2b2f5367817b707
Tim Mickel <tim@tmickel.com>
2021-06-16 13:36:16 -0400

    Replace @BFC_APP_DOMAIN@ with env var in package.json and title

--- bigfuncloud/setup-template.sh
+++ bigfuncloud/setup-template.sh
@@ -9,3 +9,6 @@ SECRET=$(python -c "import secrets; print(secrets.token_urlsafe())")
 echo "DJANGO_SECRET_KEY=\"$SECRET\"" > .envrc.secret
 
 direnv allow
+
+sd "@BFC_APP_DOMAIN@" "$BFC_APP_DOMAIN" package.json
+sd "@BFC_APP_DOMAIN@" "$BFC_APP_DOMAIN" ./frontend/index.html
